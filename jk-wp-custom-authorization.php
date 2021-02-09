f75d01d9229629afc4f776e8c49195da2b8d4faa
Alexander Poslushnyak <fr4nk.horr1gan@gmail.com>
2021-02-09 04:50:31 +0200

    Added initial ajax handlers

--- jk-wp-custom-authorization.php
+++ jk-wp-custom-authorization.php
@@ -3,10 +3,30 @@
 class jk_wp_custom_authorization
 {
 
-    public function init()
+    public function authorization_handler()
     {
 
+        die();
 
+    }
+
+    public function registration_handler()
+    {
+
+        die();
+
+    }
+
+    public function ajax_init()
+    {
+
+        add_action('wp_ajax_nopriv_jk_authorization', [$this, 'authorization_handler']);
+
+        add_action('wp_ajax_jk_authorization', [$this, 'authorization_handler']);
+
+        add_action('wp_ajax_nopriv_jk_registration', [$this, 'registration_handler']);
+
+        add_action('wp_ajax_jk_registration', [$this, 'registration_handler']);
 
     }
 
